a20ff83a54f82437d2bcb65fd5c3edabeb98d787
Tanto <zr380678644997@gmail.com>
2021-11-04 14:55:30 +0300

    Something that looks like a MAp

--- Client_Tank/Game1.cs
+++ Client_Tank/Game1.cs
@@ -14,10 +14,12 @@ namespace Client_Tank
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D tank_texture;
+        Texture2D map_texture;
         Tank tank = new Tank(300, 3, 30);
         Vector2 position = Vector2.Zero;
         Client client = new Client("127.0.0.1", 8000);
         List<Tank> tanks = new List<Tank>();
+        Map mapp = new Map(12,12);
         //List<Texture2D> texture2Ds = new List<Texture2D>();
 
         public Game1()
@@ -30,8 +32,13 @@ namespace Client_Tank
 
         protected override void Initialize()
         {
+            _graphics.PreferredBackBufferWidth = 560;
+            _graphics.PreferredBackBufferHeight = 560;
+            _graphics.ApplyChanges();
             var rand = new Random();
             // TODO: Add your initialization logic here
+            mapp.DefaultMapCreate();
+           
             client.ID = rand.Next(1, 10);
             client.CreateIPEndPoint();
             client.Conect();
@@ -46,6 +53,9 @@ namespace Client_Tank
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             tank_texture = Content.Load<Texture2D>(@"Texture\Tank");
+            map_texture = Content.Load<Texture2D>(@"Texture\wall");
+            tank.X = 70;
+            tank.Y = 70;
             // TODO: use this.Content to load your game content here
         }
 
@@ -110,8 +120,26 @@ namespace Client_Tank
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
+            int rows = mapp.map.GetUpperBound(0) + 1;    // количество строк
+            int columns =mapp.map.Length / rows;        // количество столбцов
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < mapp.wight; i++)
+            {
+                for (int j = 0; j < mapp.height; j++)
+                {
+                    if (mapp.map[i,j]=='X')
+                    {
+                        _spriteBatch.Draw(map_texture, new Rectangle(x, y, 50, 50), null, Color.White, 0, new Vector2(40 / 2f, 49 / 2f), SpriteEffects.None, 0f);
+                        x = i*50;
+                        y = j*50;
+                    }
+                    
 
+                }
 
+            }
+            _spriteBatch.Draw(map_texture, new Rectangle(550, 550, 50, 50), null, Color.White, 0, new Vector2(40 / 2f, 49 / 2f), SpriteEffects.None, 0f);
             //_spriteBatch.Draw(tank_texture, new Rectangle(tank.X, tank.Y, 40, 49), null, Color.White, tank.Rotation, new Vector2(40 / 2f, 49 / 2f), SpriteEffects.None, 0f);
 
             foreach (var item in tanks)
---
+++ Tank_Server_Client_lib/Map.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tank_Server_Client_lib
+{
+    public class Map
+    {
+        public int wight { get; set; }
+        public int height { get; set; }
+
+        public char[,] map { get; set; }
+
+        public Map(int wight, int height)
+        {
+
+            this.wight = wight;
+            this.height = height;
+        }
+
+
+        public void DefaultMapCreate()
+        {
+            this.map = new char[this.wight,this.height];
+            int rows = this.map.GetUpperBound(0) + 1;    // количество строк
+            int columns = this.map.Length / rows;        // количество столбцов
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    this.map[i, j] = ' ';
+                }
+
+            }
+
+            for (int i = 0; i < this.wight; i++)
+            {
+                this.map[0,i] = 'X';
+            }
+
+            for (int i = 0; i < this.wight; i++)
+            {
+                this.map[this.height-1, i] = 'X';
+            }
+
+            for (int i = 0; i < this.height; i++)
+            {
+                this.map[i,0] = 'X';
+            }
+
+            for (int i = 0; i < this.height; i++)
+            {
+                this.map[i, this.wight-1] = 'X';
+            }
+        }
+
+        
+
+
+    }
+}
